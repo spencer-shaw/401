feb2fc5208ea1f9c27a9e5d2da4a1024cf0c4d87
Spencer Shaw <w0413731@selu.edu>
2018-08-26 17:46:37 -0500

    Changed MagicCard.cs model. Added Type, MultiverseId, ManaCost and Name. Scaffold controller and views. Restricted access to the MagicCard Page(User needs to be logged in to view and add cards). Users can only view and edit the cards on their account.

--- Mtg.Card.Tracker/Mtg.Card.Tracker/Data/Migrations/20180826191247_AddNameToMagicCard.Designer.cs
+++ Mtg.Card.Tracker/Mtg.Card.Tracker/Data/Migrations/20180826191247_AddNameToMagicCard.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mtg.Card.Tracker.Data;
 
 namespace Mtg.Card.Tracker.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180826191247_AddNameToMagicCard")]
+    partial class AddNameToMagicCard
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -201,19 +203,13 @@ namespace Mtg.Card.Tracker.Data.Migrations
 
                     b.Property<string>("IdentityUserId");
 
-                    b.Property<int>("ManaCost");
-
                     b.Property<string>("MtgUserId");
-
-                    b.Property<int>("MultiverseId");
 
                     b.Property<string>("Name");
 
                     b.Property<int>("Power");
 
                     b.Property<int>("Toughness");
-
-                    b.Property<string>("Type");
 
                     b.HasKey("MagicCardId");
 
--- Mtg.Card.Tracker/Mtg.Card.Tracker/Models/MagicCard.cs
+++ Mtg.Card.Tracker/Mtg.Card.Tracker/Models/MagicCard.cs
@@ -9,10 +9,14 @@ namespace Mtg.Card.Tracker.Models
     public class MagicCard
     {
         public int MagicCardId { get; set; }
+        public string Name { get; set; }
         public string Color { get; set; }
         public int Power { get; set; }
         public int Toughness { get; set; }
         public string Description { get; set; }
+        public string Type { get; set; }
+        public int ManaCost { get; set; }
+        public int MultiverseId { get; set; }
         public string IdentityUserId { get; set; }           //Forgien Key to Identity User
         public virtual IdentityUser IdentityUser { get; set; }
     }
