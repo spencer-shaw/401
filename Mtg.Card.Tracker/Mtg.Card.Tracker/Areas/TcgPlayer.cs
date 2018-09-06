098b998c3541d32b780f29f2f1325a885c7853b9
Spencer Shaw <w0413731@selu.edu>
2018-09-05 19:18:52 -0500

    Created Class to consume api

---
+++ Mtg.Card.Tracker/Mtg.Card.Tracker/Areas/TcgPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mtg.Card.Tracker.Areas
+{
+    public class TcgPlayer
+    {
+        //TODO Consume Api here -Shaw
+    }
+}
