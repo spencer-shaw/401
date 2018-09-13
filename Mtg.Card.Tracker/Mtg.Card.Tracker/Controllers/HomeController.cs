f2fc845cc1b6bb964439410a0e28afd99aa16f75
Spencer Shaw <w0413731@selu.edu>
2018-09-13 15:09:00 -0500

    Did some things on the front

--- Mtg.Card.Tracker/Mtg.Card.Tracker/Controllers/HomeController.cs
+++ Mtg.Card.Tracker/Mtg.Card.Tracker/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Mtg.Card.Tracker.Models;
 
@@ -14,14 +15,14 @@ namespace Mtg.Card.Tracker.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
-
+        [Authorize]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
