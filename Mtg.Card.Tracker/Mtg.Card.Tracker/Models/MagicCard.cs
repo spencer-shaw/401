88aae7acfcbe8d43540c54d73df2afe019304500
spencer-shaw <spencer.shaw@selu.edu>
2018-11-07 21:24:47 -0600

    Added data annotations to MagicCard and some front end changes

--- Mtg.Card.Tracker/Mtg.Card.Tracker/Models/MagicCard.cs
+++ Mtg.Card.Tracker/Mtg.Card.Tracker/Models/MagicCard.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Mtg.Card.Tracker.Models
     public class MagicCard
     {
         public int MagicCardId { get; set; }
+        [Display(Name = "Card Name")]
         public string Name { get; set; }
         public string Color { get; set; }
         public int? Power { get; set; }
@@ -17,11 +19,15 @@ namespace Mtg.Card.Tracker.Models
         public string Description { get; set; }
         public string Type { get; set; }
         public int? ManaCost { get; set; }
+        [Display(Name = "Multiverse Id")]
         public int? MultiverseId { get; set; }
         public string IdentityUserId { get; set; }           //Forgien Key to Identity User
+        [Display(Name = "Image")]
         public string ImageUrl { get; set; }
         public virtual IdentityUser IdentityUser { get; set; }
+        [Display(Name = "Quantity")]
         public int CardsAmount { get; set; }  // Amount of certain card
+        [Display(Name = "Price USD")]
         public double Price { get; set; }
       
         [InverseProperty("CardOffer")]
